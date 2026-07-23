773039719f78ae06e9834399601506de9aa653c7
Pranesh_Ghub <145572300+MukhilPranesh2005@users.noreply.github.com>
2026-07-23 22:16:28 +0530

    Improve Armstrong number logic
    Refactor Armstrong number check to use digit count for power calculation.

--- Amstrong Number.java
+++ Amstrong Number.java
@@ -10,19 +10,37 @@ class Codechef
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int num=n;
+		int temp=n;
 		int s=0;
-		int p=-1;
-	    while(num>0)
-	    {
-	        int d=num%10;
-	        p=d*d*d;
-	        num/=10;
-	       s+=p;
-	    }
-	    //int ans=s==n? 1:0;
-	    System.out.println(n+" "+s);
-	    System.out.print(s==n);
-	  
-	   
+		//int c=String.valueOf(n).length();
+		int c=0;
+		while(temp>0)
+		{
+		     int di=temp%10;
+		    c++;
+		    temp/=10;
+		}
+        while(num>0)
+        {
+            int d=num%10;
+            int p=(int)Math.pow(d,c);
+            num/=10;
+            s+=p;
+        }
+        if(s==n)
+        {
+            System.out.print(s+" is a Amstrong number!");
+        }
+        else
+        {
+            System.out.print(s+" is not a Amstrong number!");
+        }
+        
+        
+		
+
+		
+		
+       
 	}
 }
